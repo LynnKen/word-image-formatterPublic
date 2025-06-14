14987b4c0ca54529bb6138ff63a8a606d1146007
LynnKen <lynnken04@gmail.com>
2025-06-14 18:50:50 +0300

    Update app.py

--- app.py
+++ app.py
@@ -7,11 +7,10 @@ from docx.enum.text import WD_PARAGRAPH_ALIGNMENT
 from docx.oxml import OxmlElement
 from docx.oxml.ns import qn
 import streamlit as st
-st.set_page_config(page_title="Word Report Generator", layout="centered")
-
-# הגדרת תצוגת העמוד - חובה לפני כל קריאה ל-st
 from streamlit_cropper import st_cropper
 from streamlit_drawable_canvas import st_canvas
+
+st.set_page_config(page_title="Word Report Generator", layout="centered")
 
 # פונקציית עזר לעריכת תמונה
 
